f0c85ac6219b6847eb2661057f7d0994f1397b48
Aof <63310027@go.buu.ac.th>
2023-11-12 17:56:44 +0700

    first commit

---
+++ Controller/EmployeeController.cs
@@ -0,0 +1,111 @@
+// Top-level statements (if any)
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Northwind.Data;
+using Northwind.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Northwind.MySQL.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly NorthwindContext _context;
+
+        public EmployeeController(NorthwindContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Employee>>> GetEmployees()
+        {
+            var employees = await _context.Employees.ToListAsync();
+            return employees;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Employee>> GetEmployee(int id)
+        {
+            var employee = await _context.Employees.FindAsync(id);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return employee;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Employee>> PostEmployee(Employee employee)
+        {
+            try
+            {
+                _context.Employees.Add(employee);
+                await _context.SaveChangesAsync();
+
+                return CreatedAtAction(nameof(GetEmployee), new { id = employee.EmployeeId }, employee);
+            }
+            catch (DbUpdateException ex)
+            {
+                var innerException = ex.InnerException;
+                // Log or handle the inner exception details
+                return StatusCode(500, "An error occurred while saving the entity changes.");
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutEmployee(int id, Employee employee)
+        {
+            if (id != employee.EmployeeId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(employee).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EmployeeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEmployee(int id)
+        {
+            var employee = await _context.Employees.FindAsync(id);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            _context.Employees.Remove(employee);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool EmployeeExists(int id)
+        {
+            return _context.Employees.Any(e => e.EmployeeId == id);
+        }
+    }
+}
